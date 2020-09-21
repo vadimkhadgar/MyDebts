98901fde6fde85795c4d1df8b5b2fce8e222dd85
Vadim Koroliuk <bum2000ivanov@mail.ru>
2020-09-21 21:54:00 +0300

    refactoring

--- app/src/main/java/com/mobile/mydebts/db/PaymentDao.kt
+++ app/src/main/java/com/mobile/mydebts/db/PaymentDao.kt
@@ -7,7 +7,7 @@ import com.mobile.mydebts.model.PaymentPhone
 @Dao
 interface PaymentDao {
     @Query("SELECT * FROM paymentsPhoneLoan")
-    fun  getPayments(): LiveData<List<PaymentPhone>>
+    suspend fun  getPayments(): LiveData<List<PaymentPhone>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(payment: PaymentPhone)
