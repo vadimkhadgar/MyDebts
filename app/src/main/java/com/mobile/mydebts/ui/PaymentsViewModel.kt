8093cb7029bbdeb5eaa8c34b501d14915f66777a
Vadim Koroliuk <bum2000ivanov@mail.ru>
2020-11-04 14:20:08 +0300

    improved performance

--- app/src/main/java/com/mobile/mydebts/ui/PaymentsViewModel.kt
+++ app/src/main/java/com/mobile/mydebts/ui/PaymentsViewModel.kt
@@ -2,6 +2,7 @@ package com.mobile.mydebts.ui
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.distinctUntilChanged
 import androidx.lifecycle.viewModelScope
 import com.mobile.mydebts.db.PaymentsRepository
 import com.mobile.mydebts.model.PaymentPhone
@@ -10,7 +11,7 @@ import kotlinx.coroutines.launch
 class PaymentsViewModel @ViewModelInject constructor(private val paymentsRepository: PaymentsRepository) :
     ViewModel() {
 
-    fun getPayments() = paymentsRepository.allPayments
+    fun getPayments() = paymentsRepository.allPayments.distinctUntilChanged() // Fixing unnecessary updates
 
     fun insertPayment(payment: PaymentPhone) =
         viewModelScope.launch { paymentsRepository.insert(payment) }
